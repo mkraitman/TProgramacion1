c5f8deff1ad628e92e9a3b74c4a29fcba54a815e
mkraitman <mkraitman@gmail.com>
2019-03-19 16:55:11 -0300

    tp1

---
+++ TP1_Taller/src/entidades/Integrante.java
@@ -0,0 +1,26 @@
+package entidades;
+
+public class Integrante {
+
+	private String nombre;
+
+	public Integrante(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Integrante [nombre=" + nombre + "]";
+	}
+	
+	
+}
